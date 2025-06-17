49126d0e893e396580320bd359a8e8bcf5b003c1
DEEPALIYADAV28 <ydeepali52@gmail.com>
2025-06-17 14:51:12 +0530

    frontend setup done

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+const App=()=>{
+  return(
+    <div className='flex justify-center item-centermin-h-screen text-3xl font-extrabold'>
+      hello world
+    </div>
+  )
+}
+export default App
